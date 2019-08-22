72d822fc2c46d55d959cc8d2f50145d8607e7126
Jorge Timón <jtimon@jtimon.cc>
2019-08-22 18:17:31 +0200

    Tests: Use self.chain instead of 'regtest' in all current tests

--- test/functional/tool_wallet.py
+++ test/functional/tool_wallet.py
@@ -196,7 +196,7 @@ class ToolWalletTest(BitcoinTestFramework):
         self.log.debug('Wallet file shasum unchanged\n')
 
     def run_test(self):
-        self.wallet_path = os.path.join(self.nodes[0].datadir, 'regtest', 'wallets', 'wallet.dat')
+        self.wallet_path = os.path.join(self.nodes[0].datadir, self.chain, 'wallets', 'wallet.dat')
         self.test_invalid_tool_commands_and_args()
         # Warning: The following tests are order-dependent.
         self.test_tool_wallet_info()
